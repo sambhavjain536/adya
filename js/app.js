17d3e004a1f9008a063268ad5b8ca9b577bba897
Ayrton De Craene <ayrton.decraene@gmail.com>
2013-06-09 19:33:24 +0200

    Use fixture adapter for data

--- js/app.js
+++ js/app.js
@@ -9,3 +9,28 @@ App.IndexRoute = Ember.Route.extend({
     return ['red', 'yellow', 'blue'];
   }
 });
+
+App.DiscographyRoute = Ember.Route.extend({
+  model: function() {
+    return App.Album.find();
+  }
+});
+
+App.Store = DS.Store.extend({
+  adapter: 'DS.FixtureAdapter'
+});
+
+App.Album = DS.Model.extend({
+  title: DS.attr('string')
+});
+
+App.Album.FIXTURES = [
+  {
+    id: 1,
+    title: 'Classic 1'
+  },
+  {
+    id: 2,
+    title: 'Classic 2'
+  }
+];
