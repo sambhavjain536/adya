82d85a24e62d4fd13e377b5a1fb13731c2cba705
Ayrton De Craene <ayrton.decraene@gmail.com>
2013-06-10 13:42:25 +0200

    Add tracks for Adya Classic 2

--- js/app.js
+++ js/app.js
@@ -50,7 +50,8 @@ App.Album.FIXTURES = [
   },
   {
     id: 2,
-    title: 'Classic 2'
+    title: 'Classic 2',
+    tracks: [21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37]
   },
   {
     id: 3,
@@ -150,5 +151,90 @@ App.Track.FIXTURES = [
     id: 17,
     title: 'Il Trovatore Quodis Es Vitar',
     composer: 'G. Verdi'
+  },
+  {
+    id: 21,
+    title: 'Toreador Atjenki',
+    composer: 'G. Bizet'
+  },
+  {
+    id: 22,
+    title: 'Alla Turca Ahriahne',
+    composer: 'W.A. Mozart'
+  },
+  {
+    id: 23,
+    title: 'Morning Mood Trevobima',
+    composer: 'W.A. Mozart'
+  },
+  {
+    id: 24,
+    title: 'Orpheus In The Underworld Nagodus',
+    composer: 'P. Sterman'
+  },
+  {
+    id: 25,
+    title: 'Amazing Grace Siralynth',
+    composer: 'E. Abrath'
+  },
+  {
+    id: 26,
+    title: 'Hungarian Dance Netonkla',
+    composer: 'J. Brahms'
+  },
+  {
+    id: 27,
+    title: 'Barcarolle Ourybova',
+    composer: 'J.Offenbach'
+  },
+  {
+    id: 28,
+    title: 'The Emperor Muvagora',
+    composer: 'J. Strauss - Son'
+  },
+  {
+    id: 29,
+    title: 'Dance Of The Hours Souraktoff',
+    composer: 'A. Ponchielli'
+  },
+  {
+    id: 30,
+    title: 'Air Dorilun',
+    composer: 'J.S. Bach'
+  },
+  {
+    id: 31,
+    title: 'Aïda Solaekride',
+    composer: 'G. Verdi'
+  },
+  {
+    id: 32,
+    title: 'Capriccio Italien Fagrozzi',
+    composer: 'P.I. Tchaikovsky'
+  },
+  {
+    id: 33,
+    title: 'Going Home Midecras',
+    composer: 'A.L. Dvorak',
+  },
+  {
+    id: 34,
+    title: 'Serenade Lakisong',
+    composer: 'J. Haydn'
+  },
+  {
+    id: 35,
+    title: 'Swan Lake Ositares',
+    composer: 'P.I. Tchaikovsky'
+  },
+  {
+    id: 36,
+    title: 'Harp Concerto Endasori',
+    composer: 'G.F. Händel'
+  },
+  {
+    id: 37,
+    title: 'Italian Symphony Krilasko',
+    composer: 'F. Mendelssohn, Bartholdy'
   }
 ];
